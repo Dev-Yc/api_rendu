9764dc13563e85f397cc512788047dde502d0150
Dev-Yc <maid-cafe@hotmail.fr>
2023-09-29 22:15:07 +0200

    Ajout de la synchronisation dans le même port

--- app.js
+++ app.js
@@ -8,13 +8,38 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
+let currentPlayer = 'X';
+
+function isGameOver(board, row, col) {
+    const player = board[row][col];
+    if (board[row].every(cell => cell === player)) return true;
+    if (board.every(row => row[col] === player)) return true;
+    if (row === col && board[0][0] === player && board[1][1] === player && board[2][2] === player) return true;
+    if (row + col === 2 && board[0][2] === player && board[1][1] === player && board[2][0] === player) return true;
+    if (board.flat().every(cell => cell !== '')) return 'draw'; // retourne 'draw' si le tableau est plein
+    return false;
+}
+
+let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+
 io.on('connection', (socket) => {
-    console.log('New client connected');
     socket.on('play', (data) => {
-        socket.broadcast.emit('play', data);
-    });
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
+        board[data.row][data.col] = currentPlayer;
+        const gameResult = isGameOver(board, data.row, data.col);
+        if (gameResult) {
+            io.emit('gameOver', { winner: gameResult === 'draw' ? 'Draw' : currentPlayer });
+            board = [
+                ['', '', ''],
+                ['', '', ''],
+                ['', '', '']
+            ];
+        }
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        io.emit('play', { ...data, player: currentPlayer });
     });
 });
 
